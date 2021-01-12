40b766785e48fbc9067badeeba893779b923929c
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2021-01-12 13:32:26 +0300

    Added PlaceModel

--- Places/MainViewController.swift
+++ Places/MainViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
-    let restaurantNames = [
-        "Mirazur", "Noma 2.0", "Asador Etxebarri", "Gaggan",
-        "Geranium", "Central", "Mugaritz", "Arpege",
-        "Disfrutar", "Maido"
-    ]
+//    let restaurantNames = [
+//        "Mirazur", "Noma 2.0", "Asador Etxebarri", "Gaggan",
+//        "Geranium", "Central", "Mugaritz", "Arpege",
+//        "Disfrutar", "Maido"
+//    ]
 
+    let places = Place.getPlaces()
+    // Меняем массив [Place(name: "Mirazur", location: "France", type: "Restaurant", image: "Mirazur")] на Place.getPlaces()
+    // Change the array [Place (name: "Mirazur", location: "France", type: "Restaurant", image: "Mirazur")] to Place.getPlaces ()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,7 +28,7 @@ class MainViewController: UITableViewController {
     // Метод numberOfRowsInSection позволяет отобразить колличество строк необходимое для работы с таблицей
     // Method numberOfRowsInSection allows you to display the number of rows required to work with the table
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
     // В данном методе cellForRowAt indexPath работаем над контентом который отображается в самой ячейке
     // In this method, cellForRowAt indexPath, we work on the content that is displayed in the cell itself
@@ -35,10 +38,14 @@ class MainViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell // приводим объект cell к нашему классу as! CustomTableViewCell
             // cast the cell object to our CustomTableViewCell class
 
-        cell.nameLable.text = restaurantNames[indexPath.row] // меняем свойства родительского класса UITableViewCell а именно textLabel? на cвойства нашего класса CustomTableViewCell а именно nameLable то есть на те аутлеты которые мы создали
-        //  change the properties of the parent class UITableViewCell namely textLabel? to the properties of our CustomTableViewCell class, namely nameLable, that is, to those outlets that we created
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row]) // тут меняем imageView? на imageOfPlace ниже так же меняем на imageOfPlace
-        //  change the imageView here? on imageOfPlace below also change to imageOfPlace
+        cell.nameLable.text = places[indexPath.row].name // 2. меняем массив restaurantNames на places и в конце добавляем свойство .name так из массива places мы вытаскиваем конкретно объект name 1.меняем свойства родительского класса UITableViewCell а именно textLabel? на cвойства нашего класса CustomTableViewCell а именно nameLable то есть на те аутлеты которые мы создали
+        //  2. change the array restaurantNames to places and at the end add the .name property so from the places array we pull out the name object specifically 1. change the properties of the parent class UITableViewCell namely textLabel? to the properties of our CustomTableViewCell class, namely nameLable, that is, to those outlets that we created
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image) // 2. меняем массив restaurantNames на places и в конце добавляем свойство .image так из массива places мы вытаскиваем конкретно объект image 1. тут меняем imageView? на imageOfPlace ниже так же меняем на imageOfPlace
+        //  2. change the array restaurantNames to places and at the end add the .image property so from the places array we pull out the image object specifically 1. change the imageView here? on imageOfPlace below also change to imageOfPlace
+        cell.locationLable.text = places[indexPath.row].location // Присваиваем свойству location конкретное значение
+        //  Assigning a specific value to the location property
+        cell.typeLable.text = places[indexPath.row].type // Присваиваем свойству type конкретное значение
+        //  Assigning a specific value to the type property
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2   // Для того чтобы сделать прямоугольники с скругленными краями ставим 65 / 2 вместо cell.imageOfPlace.frame.size.height / 2
         //  In order to make rectangles with rounded edges, put 65/2 instead of cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true // обрезаем изображение по границам imageView при помощи свойсва .clipsToBounds
@@ -50,8 +57,8 @@ class MainViewController: UITableViewController {
     // MARK: - Table View Delegate
     // Метод который определяет высоту строк heightForRowAt indexPath
     // The method that determines the height of the rows: heightForRowAt indexPath
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
+    //override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    //  return 85   Высоту строки мы настроили через Интерфейсбилдер поэтому данные строки кода можно удалять, но я оставлю для общего понимания
     }
 
     /*
@@ -64,4 +71,4 @@ class MainViewController: UITableViewController {
     }
     */
 
-}
+
---
+++ Places/PlaceModel.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceModel.swift
+//  Places
+//
+//  Created by Alexander Sobolev on 12.01.2021.
+//
+
+import Foundation
+
+struct Place { // Создаем модель для возможности добавления записей названия, местоположения, типа а так же изображения в нашем приложении
+               // We create a model for the ability to add records of name, location, type, as well as an image in our application
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [ // тут также ставим static // here we also put static
+            "Mirazur", "Noma 2.0", "Asador Etxebarri", "Gaggan",
+            "Geranium", "Central", "Mugaritz", "Arpege",
+            "Disfrutar", "Maido"
+        ]
+    
+    static func getPlaces() -> [Place] { // при добавлении static делаем этот метод статичным или по другому выражаясь методом структуры, в этом случаее данный метод будет доступен не через экземпляр структуры а при непосредственном обращении к самой структуре и те свойства к которым мы обращаемся тоже должны быть статичными тоесть наш массив restaurantNames
+    //  when adding static, we make this method static or, in other words, the structure method, in this case, this method will be available not through an instance of the structure, but when directly accessing the structure itself, and the properties to which we refer must also be static, that is, our restaurantNames array
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "France", type: "Restaurant", image: place))
+        }
+        
+        return places
+    }
+    
+}
